39830b7ff94401490aa6db96f6f8cbf96ecb1ec7
wendy <mizukilee@gmail.com>
2022-12-04 20:47:11 -0500

    commit tree c01ea54d0c524bc0ff60d6d67fea5a60aa6a9614

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+
+function submitData(user, email) {
+    // function configObject() {
+    //     method: 'POST'
+    //     headers: {
+    //         'Content-Type': 'application/json', 
+    //         'Accept': 'application/json' 
+    //     }
+    //     body: ({
+    //         username: 'user',
+    //         emailAddress: 'email'
+    //     }) 
+    
+    fetch('http://localhost:3000/users', configObject) 
+        .then(resp => resp.json())
+        .then(object => console.log(object))
+}
+// post request using fetch 
+// function that takes in name and email 
+// fetch inside sumbit data --> sumbitdata returns the fetch
